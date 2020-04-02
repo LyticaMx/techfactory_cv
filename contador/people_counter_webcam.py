9a4de4cd8863ccb639127ea1c7153a18b8d4655c
soyjorge <jorge.martinez@lytica.ai>
2020-04-02 13:42:22 -0600

    ctrl-c keyboard interrupt

--- contador/people_counter_webcam.py
+++ contador/people_counter_webcam.py
@@ -128,221 +128,225 @@ totalUp = 0
 # start the frames per second throughput estimator
 fps = FPS().start()
 
-# loop over frames from the video stream
-while True:
-	# grab the next frame and handle if we are reading from either
-	# VideoCapture or VideoStream
-	frame = vs.read()
+try:
+	# loop over frames from the video stream
+	while True:
+		# grab the next frame and handle if we are reading from either
+		# VideoCapture or VideoStream
+		frame = vs.read()
 
-	# resize the frame to have a maximum width of 500 pixels (the
-	# less data we have, the faster we can process it), then convert
-	# the frame from BGR to RGB for dlib
-	frame = imutils.resize(frame, width=500)
-	if args["camera"] == 'f':
-		frame = cv2.flip(frame, -1)
-	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		# resize the frame to have a maximum width of 500 pixels (the
+		# less data we have, the faster we can process it), then convert
+		# the frame from BGR to RGB for dlib
+		frame = imutils.resize(frame, width=500)
+		if args["camera"] == 'f':
+			frame = cv2.flip(frame, -1)
+		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+		# if the frame dimensions are empty, set them
+		if W is None or H is None:
+			(H, W) = frame.shape[:2]
 
-	# if the frame dimensions are empty, set them
-	if W is None or H is None:
-		(H, W) = frame.shape[:2]
+		# initialize the current status along with our list of bounding
+		# box rectangles returned by either (1) our object detector or
+		# (2) the correlation trackers
+		status = "En espera"
+		rects = []
 
-	# initialize the current status along with our list of bounding
-	# box rectangles returned by either (1) our object detector or
-	# (2) the correlation trackers
-	status = "En espera"
-	rects = []
+		# check to see if we should run a more computationally expensive
+		# object detection method to aid our tracker
+		if totalFrames % args["skip_frames"] == 0:
+			# set the status and initialize our new set of object trackers
+			status = "Detectando..."
+			trackers = []
 
-	# check to see if we should run a more computationally expensive
-	# object detection method to aid our tracker
-	if totalFrames % args["skip_frames"] == 0:
-		# set the status and initialize our new set of object trackers
-		status = "Detectando..."
-		trackers = []
+			# convert the frame to a blob and pass the blob through the
+			# network and obtain the detections
+			blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+			net.setInput(blob)
+			detections = net.forward()
 
-		# convert the frame to a blob and pass the blob through the
-		# network and obtain the detections
-		blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
-		net.setInput(blob)
-		detections = net.forward()
+			# loop over the detections
+			for i in np.arange(0, detections.shape[2]):
+				# extract the confidence (i.e., probability) associated
+				# with the prediction
+				confidence = detections[0, 0, i, 2]
 
-		# loop over the detections
-		for i in np.arange(0, detections.shape[2]):
-			# extract the confidence (i.e., probability) associated
-			# with the prediction
-			confidence = detections[0, 0, i, 2]
+				# filter out weak detections by requiring a minimum
+				# confidence
+				if confidence > args["confidence"]:
+					# extract the index of the class label from the
+					# detections list
+					idx = int(detections[0, 0, i, 1])
 
-			# filter out weak detections by requiring a minimum
-			# confidence
-			if confidence > args["confidence"]:
-				# extract the index of the class label from the
-				# detections list
-				idx = int(detections[0, 0, i, 1])
+					# if the class label is not a person, ignore it
+					# if CLASSES[idx] != "person":
+					#	continue
 
-				# if the class label is not a person, ignore it
-				#if CLASSES[idx] != "person":
-				#	continue
+					# compute the (x, y)-coordinates of the bounding box
+					# for the object
+					box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+					(startX, startY, endX, endY) = box.astype("int")
 
-				# compute the (x, y)-coordinates of the bounding box
-				# for the object
-				box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-				(startX, startY, endX, endY) = box.astype("int")
+					# construct a dlib rectangle object from the bounding
+					# box coordinates and then start the dlib correlation
+					# tracker
+					tracker = dlib.correlation_tracker()
+					rect = dlib.rectangle(startX, startY, endX, endY)
+					tracker.start_track(rgb, rect)
 
-				# construct a dlib rectangle object from the bounding
-				# box coordinates and then start the dlib correlation
-				# tracker
-				tracker = dlib.correlation_tracker()
-				rect = dlib.rectangle(startX, startY, endX, endY)
-				tracker.start_track(rgb, rect)
+					# add the tracker to our list of trackers so we can
+					# utilize it during skip frames
+					trackers.append(tracker)
 
-				# add the tracker to our list of trackers so we can
-				# utilize it during skip frames
-				trackers.append(tracker)
-
-	# otherwise, we should utilize our object *trackers* rather than
-	# object *detectors* to obtain a higher frame processing throughput
-	else:
-		# loop over the trackers
-		for tracker in trackers:
-			# set the status of our system to be 'tracking' rather
-			# than 'waiting' or 'detecting'
-			status = "Rastreando..."
-
-			# update the tracker and grab the updated position
-			tracker.update(rgb)
-			pos = tracker.get_position()
-
-			# unpack the position object
-			startX = int(pos.left())
-			startY = int(pos.top())
-			endX = int(pos.right())
-			endY = int(pos.bottom())
-
-			# add the bounding box coordinates to the rectangles list
-			rects.append((startX, startY, endX, endY))
-
-	# use the centroid tracker to associate the (1) old object
-	# centroids with (2) the newly computed object centroids
-	objects = ct.update(rects)
-
-	# loop over the tracked objects
-	for (objectID, centroid) in objects.items():
-		# check to see if a trackable object exists for the current
-		# object ID
-		to = trackableObjects.get(objectID, None)
-
-		# if there is no existing trackable object, create one
-		if to is None:
-			to = TrackableObject(objectID, centroid)
-
-		# otherwise, there is a trackable object so we can utilize it
-		# to determine direction
+		# otherwise, we should utilize our object *trackers* rather than
+		# object *detectors* to obtain a higher frame processing throughput
 		else:
-			# the difference between the y-coordinate of the *current*
-			# centroid and the mean of *previous* centroids will tell
-			# us in which direction the object is moving (negative for
-			# 'up' and positive for 'down')
-			y = [c[0] for c in to.centroids]
-			direction = centroid[0] - np.mean(y)
-			to.centroids.append(centroid)
+			# loop over the trackers
+			for tracker in trackers:
+				# set the status of our system to be 'tracking' rather
+				# than 'waiting' or 'detecting'
+				status = "Rastreando..."
 
-			# check to see if the object has been counted or not
-			if not to.counted:
-				# if the direction is negative (indicating the object
-				# is moving up) AND the centroid is above the center
-				# line, count the object
-				if direction < 0 and centroid[0] < W // 2:
-					totalUp += 1
-					to.counted = True
+				# update the tracker and grab the updated position
+				tracker.update(rgb)
+				pos = tracker.get_position()
 
-				# if the direction is positive (indicating the object
-				# is moving down) AND the centroid is below the
-				# center line, count the object
-				elif direction > 0 and centroid[0] > W // 2:
-					totalDown += 1
-					to.counted = True
+				# unpack the position object
+				startX = int(pos.left())
+				startY = int(pos.top())
+				endX = int(pos.right())
+				endY = int(pos.bottom())
 
-		# store the trackable object in our dictionary
-		trackableObjects[objectID] = to
+				# add the bounding box coordinates to the rectangles list
+				rects.append((startX, startY, endX, endY))
 
-		if writer:
-			# draw a horizontal line in the center of the frame -- once an
-			# object crosses this line we will determine whether they were
-			# moving 'up' or 'down'
-			if args["orientation"] == 'v':
-				cv2.line(frame, (W // 2, 0), (W // 2, H), (0, 255, 255), 2)
+		# use the centroid tracker to associate the (1) old object
+		# centroids with (2) the newly computed object centroids
+		objects = ct.update(rects)
+
+		# loop over the tracked objects
+		for (objectID, centroid) in objects.items():
+			# check to see if a trackable object exists for the current
+			# object ID
+			to = trackableObjects.get(objectID, None)
+
+			# if there is no existing trackable object, create one
+			if to is None:
+				to = TrackableObject(objectID, centroid)
+
+			# otherwise, there is a trackable object so we can utilize it
+			# to determine direction
 			else:
-				cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
+				# the difference between the y-coordinate of the *current*
+				# centroid and the mean of *previous* centroids will tell
+				# us in which direction the object is moving (negative for
+				# 'up' and positive for 'down')
+				y = [c[0] for c in to.centroids]
+				direction = centroid[0] - np.mean(y)
+				to.centroids.append(centroid)
 
-			# draw both the ID of the object and the centroid of the
-			# object on the output frame
-			text = "ID {}".format(objectID)
-			cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-			cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+				# check to see if the object has been counted or not
+				if not to.counted:
+					# if the direction is negative (indicating the object
+					# is moving up) AND the centroid is above the center
+					# line, count the object
+					if direction < 0 and centroid[0] < W // 2:
+						totalUp += 1
+						to.counted = True
 
-	# construct a tuple of information we will be displaying on the
-	# frame
-	delta_in = abs(en - totalUp)
-	delta_out = abs(out - totalDown)
-	timestamp = datetime.now().isoformat()
+					# if the direction is positive (indicating the object
+					# is moving down) AND the centroid is below the
+					# center line, count the object
+					elif direction > 0 and centroid[0] > W // 2:
+						totalDown += 1
+						to.counted = True
 
-	if args["display"] == 1:
-		# show the output frame
-		cv2.imshow("Frame", frame)
+			# store the trackable object in our dictionary
+			trackableObjects[objectID] = to
 
-	else:
-		print("Frame: {}".format(totalFrames))
-		print("In: {}, Out: {}".format(delta_in, delta_out))
+			if writer:
+				# draw a horizontal line in the center of the frame -- once an
+				# object crosses this line we will determine whether they were
+				# moving 'up' or 'down'
+				if args["orientation"] == 'v':
+					cv2.line(frame, (W // 2, 0), (W // 2, H), (0, 255, 255), 2)
+				else:
+					cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
 
-	# check to see if we should write the frame to disk
-	if writer:
-		info = [
-			("Salidas", totalUp),
-			("Entradas", totalDown),
-			("Status", status),
-		]
-		# loop over the info tuples and draw them on our frame
-		for (i, (k, v)) in enumerate(info):
-			text = "{}: {}".format(k, v)
-			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-						cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-		writer.write(frame)
+				# draw both the ID of the object and the centroid of the
+				# object on the output frame
+				text = "ID {}".format(objectID)
+				cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+							cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+				cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-	if delta_in > 0:
-		event = 'in'
-		info = {"event": event, "time": timestamp}
-		for i in range(delta_in):
-			infq.put(info)
-			frq.put(frame)
+		# construct a tuple of information we will be displaying on the
+		# frame
+		delta_in = abs(en - totalUp)
+		delta_out = abs(out - totalDown)
+		timestamp = datetime.now().isoformat()
 
-	if delta_out > 0:
-		event = 'out'
-		info = {"event": event, "time": timestamp}
-		for i in range(delta_out):
-			infq.put(info)
-			frq.put(frame)
+		if args["display"] == 1:
+			# show the output frame
+			cv2.imshow("Frame", frame)
 
-	if infq.empty() == False and totalFrames % 80 == 0:
-		sendInfo(infq)
+		else:
+			print("Frame: {}".format(totalFrames))
+			print("In: {}, Out: {}".format(delta_in, delta_out))
 
-	key = cv2.waitKey(1) & 0xFF
+		# check to see if we should write the frame to disk
+		if writer:
+			info = [
+				("Salidas", totalUp),
+				("Entradas", totalDown),
+				("Status", status),
+			]
+			# loop over the info tuples and draw them on our frame
+			for (i, (k, v)) in enumerate(info):
+				text = "{}: {}".format(k, v)
+				cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+							cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+			writer.write(frame)
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		time.sleep(0.01)
-		break
+		if delta_in > 0:
+			event = 'in'
+			info = {"event": event, "time": timestamp}
+			for i in range(delta_in):
+				infq.put(info)
+				frq.put(frame)
 
-	# increment the total number of frames processed thus far and
-	# then update the FPS counter
-	totalFrames += 1
-	en = totalUp
-	out = totalDown
-	fps.update()
+		if delta_out > 0:
+			event = 'out'
+			info = {"event": event, "time": timestamp}
+			for i in range(delta_out):
+				infq.put(info)
+				frq.put(frame)
 
-	if datetime.now().hour >= 20:
-		time.sleep(0.01)
-		break
+		if infq.empty() == False and totalFrames % 80 == 0:
+			sendInfo(infq)
+
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			time.sleep(0.01)
+			break
+
+		# increment the total number of frames processed thus far and
+		# then update the FPS counter
+		totalFrames += 1
+		en = totalUp
+		out = totalDown
+		fps.update()
+
+		if datetime.now().hour >= 20:
+			time.sleep(0.01)
+			break
+
+except KeyboardInterrupt:
+	print("Press Ctrl-C to terminate while statement")
+	pass
 
 if not infq.empty():
 	sendInfo(infq)
